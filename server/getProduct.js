b6c1441508b5db7c4de2a0b107c9b4103c925aa3
skylar siggard <siggardskylar@gmail.com>
2019-09-07 11:19:29 -0600

    done

--- server/getProduct.js
+++ server/getProduct.js
@@ -12,15 +12,6 @@ const getProduct = (req , res) => {
     console.log(id)
 }
 
-
-// const getProduct = (req, res) => {
-//     const item = products.find(val => val.id === parseInt(req.params.id));
-//     if (!item) {
-//         return res.status(500).send("Item not in list");
-//     }
-//     res.status(200).send(item);
-// }
-
 module.exports = getProduct
 
 
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ app.get('/api/products', getProducts)
 app.get('/api/product/:id', getProduct)
 
 
+
 app.listen(SERVER_PORT, () => {
     console.log(`Listening on port ${SERVER_PORT}`)
 })
